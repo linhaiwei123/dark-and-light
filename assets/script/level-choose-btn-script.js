5d633e117cae7a0f391babbaf642323db678746c
linhaiwei123 <15986711808@163.com>
2017-04-08 10:24:56 +0800

    fix scene multi-load at the same time

--- assets/script/level-choose-btn-script.js
+++ assets/script/level-choose-btn-script.js
@@ -2,30 +2,41 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        _levelMask: null,
+        //_levelMask: null,
         _sceneLoading: false,
         level: 0,
     },
 
     onLoad: function () {
+        this.node.on('touchstart',this.onTouchStart,this)
         if(window.levelRecord == undefined){
             window.levelRecord = {};
         }
-        if(Object.keys(window.levelRecord).length >= 9 && this.node.name == 'level-9-btn'){
+        if(this.node.name == 'level-9-btn' && Object.keys(window.levelRecord).length >= 9){
             //this.node.active = true;
             this.node.scaleX = 1;
+        }
+        if(window.levelRecord['level-' + this.level] == true){
+            this.node.color = cc.Color.BLACK;
         }
         // if(Object.keys(window.levelRecord).length >= 10){
         //     //彩蛋？
         // }
 
-        this.node.on('touchstart',this.onTouchStart,this)
+        
     },
 
     onTouchStart: function(){
+        //cc.find('Canvas').emit('level-loading');
+        //this.loadingMask.active = true;
+        console.log('loading' + this.level);
         if(!this._sceneLoading){
             this._sceneLoading = true;
-            cc.director.loadScene('level-' +this.level+ '-scene');
+            
+            // this.scheduleOnce(function(){
+                 cc.director.loadScene('level-' +this.level+ '-scene');
+            // }.bind(this),0);
+            
         }
     }
 
